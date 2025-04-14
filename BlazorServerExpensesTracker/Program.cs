2135b607c0d3b89c4e26d98d08e4a7ac9773b91c
jason-jcnb <jason-jcnb@users.noreply.github.com>
2025-04-14 23:11:45 +0300

    Initial commit

--- BlazorServerExpensesTracker/Program.cs
+++ BlazorServerExpensesTracker/Program.cs
@@ -9,7 +9,7 @@ using Syncfusion.Blazor.Popups;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
@@ -22,11 +22,11 @@ builder.Services.AddScoped<ServiceInterface, ServiceImplementation>();
 builder.Services.AddScoped<SfDialogService>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
     app.UseHsts();
 }
 
